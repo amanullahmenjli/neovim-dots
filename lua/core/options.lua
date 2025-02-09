b72349e52d68dc28afe0c582ab384d6cb9996fb0
Amanullah <menjliamanullah@egg.tn>
2025-02-09 16:38:52 +0100

    feat: set tab size to 2 for dart files

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -6,6 +6,16 @@ vim.cmd("set tabstop=4")
 vim.cmd("set softtabstop=4")
 vim.cmd("set shiftwidth=4")
 
+-- set tab size to 2 for dart files
+vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
+	pattern = { "*.dart" },
+	callback = function()
+		vim.cmd("set tabstop=2")
+		vim.cmd("set softtabstop=2")
+		vim.cmd("set shiftwidth=2")
+	end,
+})
+
 -- enable line numbers
 vim.cmd("set number")
 -- vim.wo.relativenumber = true -- make the line numbers relative
