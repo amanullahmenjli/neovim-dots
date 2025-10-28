2da129f20ff2ed49fb56841f19eecfd80b276df9
Amanullah Menjli <menjliamanullah@egg.tn>
2025-10-28 16:47:29 +0100

    refactor: remove duplicated hyprlang config

--- init.lua
+++ init.lua
@@ -18,16 +18,3 @@ require("core.langs")
 
 -- load the autoclose plugin
 require("nvim-autopairs").setup()
-
--- Hyprlang LSP
-vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
-	pattern = { "*.hl", "hypr*.conf" },
-	callback = function(event)
-		print(string.format("starting hyprls for %s", vim.inspect(event)))
-		vim.lsp.start({
-			name = "hyprlang",
-			cmd = { "hyprls" },
-			root_dir = vim.fn.getcwd(),
-		})
-	end,
-})
